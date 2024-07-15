cc6f2ac450b2b8c3d0b2d30586953ddec3eb3ca8
Andrea Kevo <markovicandrea24@gmail.com>
2024-07-15 10:29:54 +0200

    add function to print the longest word

--- src/com/company/WordCounter.java
+++ src/com/company/WordCounter.java
@@ -10,13 +10,31 @@ public class WordCounter {
         System.out.println("Please enter your text");
         String input = scanner.nextLine();
         input = input.trim();
-        int counter = 0;
         String[] words = input.split(" ");
+
+        int counter = countingNumberOfTheWords(words);
+        System.out.println("Number of the words in your sentence is " + counter);
+        String longestWord = getTheLongestWord(words);
+        System.out.println(("The longest word in your sentence is " + longestWord));
+    }
+
+    public static int countingNumberOfTheWords(String[] words) {
+        int counter = 0;
         for (int i = 0; i < words.length; i++) {
             if (!words[i].isBlank()) {
                 counter++;
             }
         }
-        System.out.println(counter);
+        return counter;
+    }
+
+    public static String getTheLongestWord(String[] words){
+        String longestWord = "";
+        for (int i = 0; i < words.length; i++) {
+            if(words[i].length()>longestWord.length()){
+                longestWord = words[i];
+            }
+        }
+        return longestWord;
     }
 }
